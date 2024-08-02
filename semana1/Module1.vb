663cb2282a48c5be13ed11b8683c85a969caf6e3
szapeta <2375894860101@ingenieria.usac.edu.gt>
2024-08-02 17:32:34 -0600

    Agregar archivos de proyecto.

---
+++ semana1/Module1.vb
@@ -0,0 +1,14 @@
+﻿Module Module1
+
+    Sub Main()
+        Console.WriteLine("Escriba tu nombre:")
+        Dim nombre As String
+
+        nombre = Console.ReadLine()
+
+        Console.WriteLine("Hola " + nombre)
+
+        Console.ReadLine()
+    End Sub
+
+End Module
