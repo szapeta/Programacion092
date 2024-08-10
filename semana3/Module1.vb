4fb6515cbdd9c11f311436acc44802c8b226fff6
szapeta <2375894860101@ingenieria.usac.edu.gt>
2024-08-09 18:50:28 -0600

    Se agrega la funcion para calcular los siguientes 5 valores a un numero ingresado.

---
+++ semana3/Module1.vb
@@ -0,0 +1,27 @@
+﻿Module Module1
+
+    Sub Main()
+        'while
+        Dim numeroBase As Integer
+        Console.WriteLine("Ingrese un valor:")
+        numeroBase = CInt(Console.ReadLine())
+
+        Dim limite As Integer
+        limite = numeroBase + 5
+
+        While numeroBase < limite
+            numeroBase = numeroBase + 1
+
+            If numeroBase Mod 2 = 0 Then
+                Console.WriteLine(CStr(numeroBase) + " par")
+            Else
+                Console.WriteLine(numeroBase & " impar")
+            End If
+
+            'numeroBase += 1
+        End While
+
+        Console.ReadLine()
+    End Sub
+
+End Module
