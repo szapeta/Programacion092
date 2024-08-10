a300e05ac8509d55bc887c0fb48080ae4af1f76e
szapeta <2375894860101@ingenieria.usac.edu.gt>
2024-08-09 19:40:20 -0600

    Se agrega la funcionalidad para detectar numeros primos

--- semana3/Module1.vb
+++ semana3/Module1.vb
@@ -4,7 +4,8 @@
         'while
         Dim numeroBase As Integer
         Console.WriteLine("Ingrese un valor:")
-        numeroBase = CInt(Console.ReadLine())
+        'numeroBase = CInt(Console.ReadLine())
+        numeroBase = 5
 
         Dim limite As Integer
         limite = numeroBase + 5
@@ -20,6 +21,35 @@
 
             'numeroBase += 1
         End While
+
+        Console.WriteLine("Ejemplo de For")
+        For indice As Integer = 0 To 4
+            Console.WriteLine("El contador es: " + CStr(indice))
+        Next
+
+
+        'Programa para mostrar los numeros impares contenidos entre 2 y el valor ingresado
+        Console.WriteLine("Ingrese un valor para calcular los primos")
+        Dim numBase As Integer
+        numBase = CInt(Console.ReadLine())
+
+        '7
+        For numTemporal As Integer = 2 To numBase - 1
+
+            Dim contadorExatos = 0
+
+            For index As Integer = 1 To numTemporal
+                If numTemporal Mod index = 0 Then
+                    contadorExatos = contadorExatos + 1
+                    'contadorExatos += 1
+                End If
+            Next
+
+            If contadorExatos = 2 Then
+                Console.WriteLine(numTemporal)
+            End If
+
+        Next
 
         Console.ReadLine()
     End Sub
