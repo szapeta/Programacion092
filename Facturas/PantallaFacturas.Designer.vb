bd9dfe4909f3866bbefe6ecab20a9afe85cc9737
szapeta <2375894860101@ingenieria.usac.edu.gt>
2024-10-18 18:15:51 -0600

    Se agrega pantalla de Factura

--- Facturas/PantallaFacturas.Designer.vb
+++ Facturas/PantallaFacturas.Designer.vb
@@ -23,6 +23,8 @@ Partial Class PantallaFacturas
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.Label1 = New System.Windows.Forms.Label()
+        Me.dgvFacturas = New System.Windows.Forms.DataGridView()
+        CType(Me.dgvFacturas, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Label1
@@ -35,18 +37,29 @@ Partial Class PantallaFacturas
         Me.Label1.TabIndex = 0
         Me.Label1.Text = "Facturas"
         '
+        'dgvFacturas
+        '
+        Me.dgvFacturas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvFacturas.Location = New System.Drawing.Point(397, 36)
+        Me.dgvFacturas.Name = "dgvFacturas"
+        Me.dgvFacturas.Size = New System.Drawing.Size(734, 515)
+        Me.dgvFacturas.TabIndex = 1
+        '
         'PantallaFacturas
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(1143, 563)
+        Me.Controls.Add(Me.dgvFacturas)
         Me.Controls.Add(Me.Label1)
         Me.Name = "PantallaFacturas"
         Me.Text = "PantallaFacturas"
+        CType(Me.dgvFacturas, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents Label1 As Label
+    Friend WithEvents dgvFacturas As DataGridView
 End Class
--- Facturas/PantallaFacturas.vb
+++ Facturas/PantallaFacturas.vb
@@ -1,3 +1,9 @@
 ﻿Public Class PantallaFacturas
+    Private Sub PantallaFacturas_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim objFacturas As ConexionBD
+        objFacturas = New ConexionBD()
 
+        dgvFacturas.DataSource = objFacturas.SelectQuery("select * from Factura")
+
+    End Sub
 End Class
